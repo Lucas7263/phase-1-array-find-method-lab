63350fb1f07a0ed10a2df94fdd8a5e0a8abb90ae
Lucas <lucaswoodyard@yahoo.com>
2022-07-29 17:59:15 -0500

    commit tree 79c3242430f15f1f89d726ced0e27ec773606483

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+
+    function superbowlWin(team) {
+       for (let i = 0; i < team.length; i++) {
+            if (team[i].result === "W"){
+                return team[i].year
+            }
+            
+       }
+      
+    };
+     record.find(superbowlWin);
+   
+
+   
+
+    
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function isOdd(element, index, array) {
+    return (element % 2 === 1);
+  }
+  
+  [4, 6, 8, 10].find(isOdd); //=> undefined, not found
+  [4, 5, 8, 10].find(isOdd); //=> 5
+  [4, 5, 7, 8, 10].find(isOdd); //=> 5
+  [4, 7, 5,  8, 10].find(isOdd); //=> 7*/
